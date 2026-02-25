0fcb5f423fb77adf5619d4a6304255873a68e376
ldclaura <lauracain12345@gmail.com>
2026-02-25 18:55:36 +1100

    working on 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  for (let i = string.charAt(string.length); i > 0; i--) {
 
+  }
+    return string.charAt(string.length-2)
 };
+
+
+// return string.charAt(string.length-1)
 
 // Do not edit below this line
 module.exports = reverseString;
+
+//   if (times < 0) return "ERROR";
+//   let string = "";
+//   for (let i = 0; i < times; i++) {
+//     string += word;
+//   }
+//   return string;
