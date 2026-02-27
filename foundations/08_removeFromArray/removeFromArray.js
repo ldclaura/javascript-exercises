226ee521d29d842a3b1877cda404645c9a34f117
ldclaura <lauracain12345@gmail.com>
2026-02-27 18:49:59 +1100

    cant be bothered

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,18 +1,14 @@
-const removeFromArray = function(f, m, n, o, p) {
-    if (typeof f == "object") {
-        // if (m or n or something != "undefined") {
 
-        // }
+
+const removeFromArray = function(arr, ...args) {
+    for (let i = args.length; i > -1; i--) {
+        if (i == args) return TRUE
     }
-    // return f, m, n, o, p
+
 };
 
-//   if (times < 0) return "ERROR";
-//   let string = "";
-//   for (let i = 0; i < times; i++) {
-//     string += word;
-//   }
-//   return string;
+
+
 // removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
 // Do not edit below this line
 module.exports = removeFromArray;
